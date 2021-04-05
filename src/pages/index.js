9f6f12946de3a9dcfca9e1d60503f440feacbb0b
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-04-05 21:10:25 +0300

    Make title empty

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   return (
-    <Layout title="Una">
+    <Layout title="">
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
